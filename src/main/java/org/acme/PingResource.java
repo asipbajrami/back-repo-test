0cfaeecada8fc281bb98754539747b53f20978ec
ekosta <e.kostandini@forward.al>
2025-11-25 18:11:39 +0100

    Update PingResource.java

--- src/main/java/org/acme/PingResource.java
+++ src/main/java/org/acme/PingResource.java
@@ -38,7 +38,7 @@ public class PingResource {
     }
 
     @GET
-    @Path("/version-json-test")
+    @Path("/version-json-test-2")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getVersionJsonTest() {
         Version version = new Version(this.version);
